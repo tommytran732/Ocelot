598e035d6a8393faaf36f95dee250038cd775e0e
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2020-10-04 14:26:47 -0400

    Update secrets.ts

--- frontend/src/secrets.ts
+++ frontend/src/secrets.ts
@@ -1,4 +1,3 @@
-const isProd: boolean = window.location.hostname === 'ocelot.thientran.io';
 export const CLD_FN_BASE_URL: string = `https://ocelotbackend.thientran.io/`,
              LOGIN_CLIENT_ID: string = '373758885533-u7l9aljbkmlaa55q1u8tnug5p19lqd9u.apps.googleusercontent.com',
-             MODULE_WL_URL: string = `https://raw.githubusercontent.com/tommytran732/ocelot-settings/master/libraries${isProd ? '' : '-beta'}.json`;
+             MODULE_WL_URL: string = `https://raw.githubusercontent.com/tommytran732/ocelot-settings/master/libraries.json`;
